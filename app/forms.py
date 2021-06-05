8be82666da5a604fa8b5b1945a28e8132a2e878b
Sahaj <gpt.sahaj28@gmail.com>
2021-06-06 05:02:56 +0530

    Added Flask-wtforms and Registration page

---
+++ app/forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Email
+
+class LoginForm(FlaskForm):
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me?', default=True)
+    submit = SubmitField('Log In')
+
+
+class RegisterForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log In')
+
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,21 @@
 from app import app
+from app.forms import LoginForm, RegisterForm
 from flask import render_template
 
 @app.get('/')
 def home():
     return render_template("index.html")
 
-@app.route('/login')
+@app.route('/login', methods = ["GET", "POST"])
 def login():
-    return render_template("login.html")
+    form = LoginForm()
+    if form.validate_on_submit():
+        return form.data
+    return render_template("login.html", form=form)
+
+@app.route('/register', methods = ["GET", "POST"])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        return form.data
+    return render_template("register.html", form=form)
